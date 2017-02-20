08921123aaee11a1db17b5d1a13e4d20f9cc1b60
Sabbir Ahmed <prince534534@gmail.com>
2017-02-20 19:16:19 +0600

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,16 @@ var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 var TodoApp = require('TodoApp');
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(()=>{
+    console.log('New State', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the Table'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 $(document).foundation();
 require('style!css!sass!applicationStyle');
